5a811229b5dc0ae1b5ebafeff10e60b6064b5ace
marvellousz <pranavmurali024@gmail.com>
2026-03-30 00:13:43 +0530

    fix: updated frontend url origin

--- app/main.py
+++ app/main.py
@@ -46,7 +46,7 @@ app.add_middleware(
         "http://127.0.0.1:3001",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://gamma.vercel1.app",
+        "https://synapse-frontend-gamma.vercel.app",
     ],
     allow_origin_regex=r"^chrome-extension://.*",
     allow_credentials=True,
